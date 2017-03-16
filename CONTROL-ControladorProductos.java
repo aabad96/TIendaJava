980fc53250cd23cec111a26105304990c0a74aeb
DuqueAnjou <reda.chakor@gmail.com>
2017-03-16 18:50:11 +0100

    Update CONTROL-ControladorProductos.java

--- CONTROL-ControladorProductos.java
+++ CONTROL-ControladorProductos.java
@@ -2,6 +2,9 @@ package control;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 import model.Producto;
 
@@ -26,6 +29,8 @@ public class ControladorProductos implements ActionListener{
 		mc.listaProductos[0]=(Producto) productoNuevo;
 		
 		System.out.println("Producto añadido: "+ productoNuevo.getNombre()+ " " + productoNuevo.getdescripcion() + " " + productoNuevo.getPrecio() + " " + productoNuevo.getStock() + " " + productoNuevo.getImagen());
+		this.guardarArchivo(productoNuevo);
+		
 	}
 	}
 	/**
@@ -39,5 +44,26 @@ public class ControladorProductos implements ActionListener{
 	}
 	
 	
+	
+	
+	public boolean guardarArchivo(Producto pp) {
+		boolean bRet = true;
+		try (FileWriter fw = new FileWriter("Producto.txt")) {
+			String str = pp.getNombre() + ";" + pp.getdescripcion() + ";" + Double.toString(pp.getPrecio()) + ";" + Integer.toString(pp.getStock()) + ";" + pp.getImagen()  ;
+	     fw.write(str);
+	   // String listaProductos[] ;
+	     //	for (int i=0; i<listaProductos.length; i++) { 
+			//	System.out.println("Producto añadido: " + i + " = " + listaProductos[i]);
+				//String str1 = listaProductos[i] + " "; 
+				//fw.write(str1);
+			//}
+			
+	     
+		} catch(IOException exc) {
+			System.err.println("Error al escribir en archivo: " + exc);
+			bRet = false;
+		}
+		return bRet;
+	}
 
 }
