8265553d40618a39da1b9348f88f4e3a1fc5cc18
aabad96 <aabad96@icloud.com>
2017-03-16 10:36:32 +0100

    Update CONTROL-ControladorProductos.java

--- CONTROL-ControladorProductos.java
+++ CONTROL-ControladorProductos.java
@@ -11,15 +11,23 @@ public class ControladorProductos implements ActionListener{
 	
 	private VentanaProductos ventana;
 	private MainController mc;
+	public static final String add = "a";
+	
 
+	
 	public void actionPerformed(ActionEvent arg0) {
-
-		Producto productoNuevo = new Producto(ventana.campoNombre.getText(),ventana.campoDescripcion.getText(),ventana.campoPrecio.getText());
-		mc.listaProductos.add(productoNuevo);
 		
-		System.out.println("Producto añadido: "+productoNuevo);
+		String lista=(String)arg0.getActionCommand();
+		
+		if (lista.equals(add)){
+		
+			Producto productoNuevo = new Producto(ventana.campoNombre.getText(),ventana.campoDescripcion.getText(),ventana.campoPrecio.getText(),ventana.campoStock.getText(),ventana.campoImagen.getText());
+		
+		mc.listaProductos[0]=(Producto) productoNuevo;
+		
+		System.out.println("Producto añadido: "+ productoNuevo.getNombre()+ " " + productoNuevo.getdescripcion() + " " + productoNuevo.getPrecio() + " " + productoNuevo.getStock() + " " + productoNuevo.getImagen());
 	}
-
+	}
 	/**
 	 * @param ventana
 	 * @param mc
