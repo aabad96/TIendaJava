7687d8aec6728ffb4a66b81a70591fdffa52ed72
aabad96 <aabad96@icloud.com>
2017-03-19 19:59:36 +0100

    Update CONTROL-ControladorProductos.java

--- CONTROL-ControladorProductos.java
+++ CONTROL-ControladorProductos.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileWriter;
 import java.io.IOException;
-
+import java.util.Vector;
 
 import model.Producto;
 
@@ -16,6 +16,7 @@ public class ControladorProductos implements ActionListener{
 	private MainController mc;
 	public static final String add = "a";
 	
+	
 
 	
 	public void actionPerformed(ActionEvent arg0) {
@@ -26,10 +27,10 @@ public class ControladorProductos implements ActionListener{
 		
 			Producto productoNuevo = new Producto(ventana.campoNombre.getText(),ventana.campoDescripcion.getText(),ventana.campoPrecio.getText(),ventana.campoStock.getText(),ventana.campoImagen.getText());
 		
-		mc.listaProductos[0]=(Producto) productoNuevo;
+		mc.listaProductos.add(productoNuevo);
 		
 		System.out.println("Producto añadido: "+ productoNuevo.getNombre()+ " " + productoNuevo.getdescripcion() + " " + productoNuevo.getPrecio() + " " + productoNuevo.getStock() + " " + productoNuevo.getImagen());
-		this.guardarArchivo(productoNuevo);
+		this.guardarArchivo(mc.listaProductos);
 		
 	}
 	}
@@ -42,24 +43,25 @@ public class ControladorProductos implements ActionListener{
 		this.ventana = ventana;
 		this.mc = mc;
 	}
+
 	
 	
 	
-	
-	public boolean guardarArchivo(Producto pp) {
+	public boolean guardarArchivo(Vector<Producto> listaProductos) {
 		boolean bRet = true;
-		try (FileWriter fw = new FileWriter("Producto.txt")) {
-			String str = pp.getNombre() + ";" + pp.getdescripcion() + ";" + Double.toString(pp.getPrecio()) + ";" + Integer.toString(pp.getStock()) + ";" + pp.getImagen()  ;
-	     fw.write(str);
-	   // String listaProductos[] ;
-	     //	for (int i=0; i<listaProductos.length; i++) { 
-			//	System.out.println("Producto añadido: " + i + " = " + listaProductos[i]);
-				//String str1 = listaProductos[i] + " "; 
-				//fw.write(str1);
-			//}
+		try {
+			FileWriter fw = new FileWriter("Producto.txt");
+			String str = "";
+	
+	     for (int i=0; i < listaProductos.size(); i++){
+	    	 str = listaProductos.get(i).getNombre() + ";" + listaProductos.get(i).getdescripcion() + ";" + Double.toString(listaProductos.get(i).getPrecio()) + ";" 
+	    			 + Integer.toString(listaProductos.get(i).getStock()) + ";" + listaProductos.get(i).getImagen()  ;
+	     		fw.write(str);
+				
+			}
 			
 	     
-		} catch(IOException exc) {
+		} catch (IOException exc) {
 			System.err.println("Error al escribir en archivo: " + exc);
 			bRet = false;
 		}
